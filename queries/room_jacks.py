8c3ba7375d288e8e7b5600d6c273e3c2148288f5
spalka <spalka621@gmail.com>
2013-11-26 17:39:12 -0600

    room jacks script

---
+++ queries/room_jacks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+
+# Import the cgi module,json and initialize the database connection
+import cgi
+import dbconn
+
+def roomJacks():
+
+    # Initialize the FieldStorage object so we can get user input
+    params = cgi.FieldStorage()
+
+    # Run the query, quoting the user input
+    dbconn.send_query("""
+            SELECT
+			st_centroid(rooms.the_geom) AS center,
+			split_part(jack,'-',3) AS floor,
+			split_part(jack,'-',4) AS room
+			FROM access_points,rooms WHERE 
+			split_part(jack,'-',2)=""" +
+			dbconn.adapt(str(params['building'].value)).getquoted() + """
+			AND
+			split_part(rooms.room_id, '-', 2)=""" +
+			dbconn.adapt(str(params['building'].value)).getquoted() + """
+			AND
+			lpad(split_part(rooms.room_id, '-', 4),5,'0')=split_part(jack,'-',4)
+    """) 
+
+roomJacks()
